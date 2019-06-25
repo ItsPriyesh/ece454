5c6680500e107512c9912bd61e41a20a0e5f0873
Priyesh Patel <p99patel@uwaterloo.ca>
2019-06-25 15:49:17 -0400

    start on task1 spark

---
+++ a2/Task1.scala
@@ -0,0 +1,30 @@
+import org.apache.spark.{SparkContext, SparkConf}
+
+// please don't change the object name
+object Task1 {
+  def main(args: Array[String]) {
+    val conf = new SparkConf().setAppName("Task 1")
+    val sc = new SparkContext(conf)
+
+    val lines = sc.textFile(args(0))
+
+    // Converts input to rows of (movie (rating, userIndex))
+    val rows = lines.flatMap(line => {
+      val cols = line.split(",")
+      val movie = cols.head
+      val ratings = cols.tail
+      ratings
+        .zipWithIndex
+        .filter(!_._1.isEmpty)
+        .map { case (rating, idx) => movie -> (rating.toInt, List(idx)) }
+    });
+
+    // reduces rows to (move, rating, List(userIndex))
+    val reduced = rows.reduceByKey((acc, i) => if (i._1 >= acc._1) (acc._1, acc._2 ::: i._2) else acc)
+
+    // map to desired output (movie, users)
+    val output = reduced.map { case (movie, (_, users)) => movie + "," + users.mkString(",") }
+
+    output.saveAsTextFile(args(1))
+  }
+}
