6e4ef883c1e45ce2e51ddd8eed4ad5e7bd37a231
Priyesh Patel <p99patel@uwaterloo.ca>
2019-06-29 18:54:23 -0400

    implement cartesian join manually

--- a2/Task4.scala
+++ a2/Task4.scala
@@ -1,4 +1,4 @@
-import org.apache.spark.{SparkContext, SparkConf}
+import org.apache.spark.{SparkConf, SparkContext}
 
 // please don't change the object name
 object Task4 {
@@ -6,23 +6,25 @@ object Task4 {
     val conf = new SparkConf().setAppName("Task 4")
     val sc = new SparkContext(conf)
 
-      //    val local = sc.textFile("/Users/priyesh/Desktop/4A/ECE_454/assignments/a2/sample_input/smalldata.txt")
-    val local = sc.textFile(args(0))
+    val local = sc
+      .textFile(args(0))
       .map(line => {
         val tokens = line.split(",", -1)
         (tokens.head, tokens.tail)
       })
 
-    val broadcast = sc.broadcast(local)
+    val broadcast = sc.broadcast(local.collect())
 
     def countSimilarities(sim: Int, users: (Array[String], Array[String])): Int =
       users.zipped.foldLeft(0) { case (acc, (a, b)) => if (!a.isEmpty && !b.isEmpty && a == b) acc + 1 else acc }
 
     val output = local
-      .cartesian(broadcast.value)
-      .filter { case ((titleA, _), (titleB, _)) => titleA < titleB }
-      .map { case ((titleA, usersA), (titleB, usersB)) => s"$titleA,$titleB" -> (usersA, usersB) }
-      .aggregateByKey(0)(countSimilarities, _ + _)
+      .flatMap { case (titleA, usersA) =>
+        broadcast.value.map { case (titleB, usersB) => (titleA, usersA, titleB, usersB) }
+      }
+      .filter { case (titleA, _, titleB, _) => titleA < titleB }
+      .map { case (titleA, usersA, titleB, usersB) => s"$titleA,$titleB" -> (usersA, usersB) }
+      .aggregateByKey(0)(countSimilarities, _ + _) // TODO: check if this is faster than nested aggregation within the previous map step
 
     output.saveAsTextFile(args(1))
   }
