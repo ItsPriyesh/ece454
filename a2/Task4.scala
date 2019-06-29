2754e75294ada53d0fd94aae13d6e4ec15b4a587
Priyesh Patel <p99patel@uwaterloo.ca>
2019-06-29 18:39:43 -0400

    task 4 spark

--- a2/Task4.scala
+++ a2/Task4.scala
@@ -6,11 +6,24 @@ object Task4 {
     val conf = new SparkConf().setAppName("Task 4")
     val sc = new SparkContext(conf)
 
-    val textFile = sc.textFile(args(0))
+      //    val local = sc.textFile("/Users/priyesh/Desktop/4A/ECE_454/assignments/a2/sample_input/smalldata.txt")
+    val local = sc.textFile(args(0))
+      .map(line => {
+        val tokens = line.split(",", -1)
+        (tokens.head, tokens.tail)
+      })
 
-    // modify this code
-    val output = textFile.map(x => x);
-    
+    val broadcast = sc.broadcast(local)
+
+    def countSimilarities(sim: Int, users: (Array[String], Array[String])): Int =
+      users.zipped.foldLeft(0) { case (acc, (a, b)) => if (!a.isEmpty && !b.isEmpty && a == b) acc + 1 else acc }
+
+    val output = local
+      .cartesian(broadcast.value)
+      .filter { case ((titleA, _), (titleB, _)) => titleA < titleB }
+      .map { case ((titleA, usersA), (titleB, usersB)) => s"$titleA,$titleB" -> (usersA, usersB) }
+      .aggregateByKey(0)(countSimilarities, _ + _)
+
     output.saveAsTextFile(args(1))
   }
 }
