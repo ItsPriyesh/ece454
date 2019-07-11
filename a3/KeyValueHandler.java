ae8d69516acf77f8455920dddb74424306fde620
Priyesh Patel <p99patel@uwaterloo.ca>
2019-07-11 13:47:49 -0400

    implement watcher to determine role of current server replica

--- a3/KeyValueHandler.java
+++ a3/KeyValueHandler.java
@@ -14,11 +14,17 @@ import org.apache.curator.framework.*;
 
 
 public class KeyValueHandler implements KeyValueService.Iface {
+
+    enum Role {
+        PRIMARY, BACKUP,
+    }
+
     private Map<String, String> myMap;
     private CuratorFramework curClient;
     private String zkNode;
     private String host;
     private int port;
+    private Role role;
 
     public KeyValueHandler(String host, int port, CuratorFramework curClient, String zkNode) {
         this.host = host;
@@ -28,6 +34,7 @@ public class KeyValueHandler implements KeyValueService.Iface {
         myMap = new ConcurrentHashMap<String, String>();
     }
 
+    @Override
     public String get(String key) throws org.apache.thrift.TException {
         String ret = myMap.get(key);
         if (ret == null)
@@ -36,7 +43,16 @@ public class KeyValueHandler implements KeyValueService.Iface {
             return ret;
     }
 
+    @Override
     public void put(String key, String value) throws org.apache.thrift.TException {
         myMap.put(key, value);
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public boolean isPrimary() {
+        return role == Role.PRIMARY;
     }
 }
--- a3/StorageNode.java
+++ a3/StorageNode.java
@@ -1,19 +1,14 @@
-import java.io.*;
 import java.util.*;
 
-import KeyValueService;
 import org.apache.curator.framework.api.CuratorWatcher;
 import org.apache.thrift.*;
 import org.apache.thrift.server.*;
 import org.apache.thrift.transport.*;
 import org.apache.thrift.protocol.*;
-
+import KeyValueHandler.Role;
 import org.apache.zookeeper.*;
-import org.apache.zookeeper.data.*;
-import org.apache.curator.*;
 import org.apache.curator.retry.*;
 import org.apache.curator.framework.*;
-import org.apache.curator.utils.*;
 
 import org.apache.log4j.*;
 
@@ -48,7 +43,8 @@ public class StorageNode {
         int port = Integer.parseInt(args[1]);
         String zknode = args[3];
 
-        KeyValueService.Processor<KeyValueService.Iface> processor = new KeyValueService.Processor<>(new KeyValueHandler(host, port, curClient, zknode));
+        KeyValueHandler handler = new KeyValueHandler(host, port, curClient, zknode);
+        KeyValueService.Processor<KeyValueService.Iface> processor = new KeyValueService.Processor<>(handler);
         TServerSocket socket = new TServerSocket(port);
         TThreadPoolServer.Args sargs = new TThreadPoolServer.Args(socket);
         sargs.protocolFactory(new TBinaryProtocol.Factory());
@@ -70,15 +66,49 @@ public class StorageNode {
                 .withMode(CreateMode.EPHEMERAL_SEQUENTIAL)
                 .forPath(zknode + "/", serverId.getBytes());
 
-        curClient.getChildren()
-                .usingWatcher(new CuratorWatcher() {
-                    @Override
-                    public void process(WatchedEvent watchedEvent) throws Exception {
-                        // list of servers changed
-                        // read through and determine if this is the primary or backup
-
-                    }
-                })
+        StorageNodeListener listener = new StorageNodeListener(curClient, zknode, handler);
+        List<String> nodes = curClient.getChildren()
+                .usingWatcher(listener)
                 .forPath(zknode);
+
+        handler.setRole(listener.determineRole(nodes));
     }
+
+    private static class StorageNodeListener implements CuratorWatcher {
+
+        final CuratorFramework client;
+        final String zknode;
+        final KeyValueHandler handler;
+
+        StorageNodeListener(CuratorFramework client, String zknode, KeyValueHandler handler) {
+            this.client = client;
+            this.zknode = zknode;
+            this.handler = handler;
+        }
+
+        /*
+            The server whose child znode has the smallest name in the
+            lexicographic order is the primary. The other server (if one
+            exists) is the secondary or backup.
+         */
+        @Override
+        public void process(WatchedEvent watchedEvent) throws Exception {
+            List<String> nodes = client.getChildren().usingWatcher(this).forPath(zknode);
+            handler.setRole(determineRole(nodes));
+
+        }
+
+        Role determineRole(List<String> nodes) {
+            if (handler.isPrimary()) return Role.PRIMARY;
+
+            if (nodes.size() == 1) {
+                return Role.PRIMARY;
+            } else if (nodes.size() == 2) {
+                return Role.BACKUP;
+            } else {
+                throw new IllegalStateException("Number of storage nodes is " + nodes.size());
+            }
+        }
+    }
+
 }
