3eba42d0a53fb5320eade05a4b2b6c601fc0d603
Priyesh Patel <p99patel@uwaterloo.ca>
2019-07-26 13:47:24 -0400

    fix bug in ser/des for aggregation room state model

--- a4/A4Application.java
+++ a4/A4Application.java
@@ -1,6 +1,3 @@
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import org.apache.kafka.common.errors.SerializationException;
 import org.apache.kafka.common.serialization.Deserializer;
 import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
@@ -16,28 +13,39 @@ import java.util.*;
 
 public class A4Application {
 
-    public static class RoomState {
+    static class RoomState {
 
-        public RoomState() {
+        private Long occupancy;
+        private Long capacity;
+        private String output;
+
+        RoomState() {
         }
 
-        public RoomState(long occupancy, long capacity) {
+        RoomState(Long occupancy, Long capacity) {
             this.occupancy = occupancy;
             this.capacity = capacity;
-            System.out.println("Created RoomState " + toString());
         }
 
-        public void setOccupancy(long occupancy) {
-            this.occupancy = occupancy;
+        boolean isOk() {
+            return occupancy == null || capacity == null || occupancy <= capacity;
         }
 
-        public void setCapacity(long capacity) {
-            this.capacity = capacity;
+        boolean isOverloaded() {
+            return !isOk();
         }
 
-        long occupancy;
-        long capacity;
+        void setOutput(String output) {
+            this.output = output;
+        }
 
+        boolean hasOutput() {
+            return output != null;
+        }
+
+        String getOutput() {
+            return output;
+        }
 
         @Override
         public String toString() {
@@ -45,96 +53,22 @@ public class A4Application {
         }
     }
 
-    static class OccupancyCapacity {
-        Long occupancy;
-        Long capacity;
-
-        public OccupancyCapacity() {
-        }
-
-        public OccupancyCapacity(Long occupancy, Long capacity) {
-            this.occupancy = occupancy;
-            this.capacity = capacity;
-        }
-    }
-
     static class RoomStateSerializer implements Serializer<RoomState> {
-
-        private final ObjectMapper mapper = new ObjectMapper();
-
-        RoomStateSerializer() {
-            mapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
-        }
-
         @Override
         public byte[] serialize(String s, RoomState roomState) {
-            if (Objects.isNull(roomState)) return null;
-            try {
-                return mapper.writeValueAsBytes(roomState);
-            } catch (Exception e) {
-                throw new SerializationException(e);
-            }
+            if (roomState == null) return null;
+            return String.format("%s,%s", roomState.occupancy, roomState.capacity).getBytes();
         }
     }
 
     static class RoomStateDeserializer implements Deserializer<RoomState> {
-
-        private final ObjectMapper mapper = new ObjectMapper();
-
-        RoomStateDeserializer() {
-            mapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
-
-        }
-
         @Override
         public RoomState deserialize(String s, byte[] bytes) {
-            if (Objects.isNull(bytes)) return null;
-            try {
-                return mapper.treeToValue(mapper.readTree(bytes), RoomState.class);
-            } catch (Exception e) {
-                throw new SerializationException(e);
-            }
-        }
-    }
-
-    static class JacksonSerializer<T> implements Serializer<T> {
-        private final ObjectMapper mapper;
-
-        JacksonSerializer() {
-            this.mapper = new ObjectMapper();
-            this.mapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
-        }
-
-        @Override
-        public byte[] serialize(String s, T t) {
-            if (t == null) return null;
-            try {
-                return mapper.writeValueAsBytes(t);
-            } catch (Exception e) {
-                throw new SerializationException(e);
-            }
-        }
-    }
-
-    static class JacksonDeserializer<T> implements Deserializer<T> {
-
-        private final Class<T> valueType;
-        private final ObjectMapper mapper;
-
-        JacksonDeserializer(Class<T> valueType) {
-            this.valueType = valueType;
-            this.mapper = new ObjectMapper();
-            this.mapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
-        }
-
-        @Override
-        public T deserialize(String s, byte[] bytes) {
             if (bytes == null) return null;
-            try {
-                return mapper.treeToValue(mapper.readTree(bytes), valueType);
-            } catch (Exception e) {
-                throw new SerializationException(e);
-            }
+            String[] tokens = new String(bytes).split(",");
+            Long occupancy = !tokens[0].equals("null") ? Long.valueOf(tokens[0]) : null;
+            Long capacity = !tokens[1].equals("null") ? Long.valueOf(tokens[1]) : null;
+            return new RoomState(occupancy, capacity);
         }
     }
 
@@ -155,210 +89,48 @@ public class A4Application {
         props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
         props.put(StreamsConfig.STATE_DIR_CONFIG, stateStoreDir);
 
-        // add code here if you need any additional configuration options
+        Serde<RoomState> stateSerde = Serdes.serdeFrom(new RoomStateSerializer(), new RoomStateDeserializer());
 
         StreamsBuilder builder = new StreamsBuilder();
-
-        /*
-          roomCapacity:
-          RoomA,2
-          RoomB,2
-
-          students:
-          Student1,RoomA
-          Student2,RoomA
-
-          roomOccupancy:
-          RoomA,2
-
-         */
 
         // student -> latest room
         KTable<String, String> students = builder.table(studentTopic);
 
-        // room -> latest max capacity
+        // room -> current capacity
         KTable<String, Long> roomCapacity = builder.<String, String>table(classroomTopic)
                 .mapValues((ValueMapper<String, Long>) Long::valueOf);
 
-        // Current occupancy table: roomID, occupancy
+        // room -> current occupancy
         KTable<String, Long> roomOccupancy = students
                 .groupBy((student, room) -> KeyValue.pair(room, student))
                 .count();
 
-//        KTable<String, String> out = roomOccupancy.outerJoin(roomCapacity,
-//                (occupancy, capacity) -> {
-//                    System.out.printf("joining on room: occupancy=%s, capacity=%s\n", occupancy, capacity);
-//                    if (occupancy != null && capacity != null) { // Room exists for student
-//                        return occupancy > capacity ? String.valueOf(occupancy) : "OK";
-//                    } else {
-//                        return null;
-//                    }
-//                });
-
-        Serde<RoomState> roomStateSerde = Serdes.serdeFrom(new RoomStateSerializer(), new RoomStateDeserializer());
-        Serde<OccupancyCapacity> occCapSerde = Serdes.serdeFrom(new JacksonSerializer<>(), new JacksonDeserializer<>(OccupancyCapacity.class));
-
-        final RoomState state = new RoomState();
-
-        KTable<String, RoomState> c = roomOccupancy
+        /*
+         * 1. Join current occupancy with current capacity,
+         * 2. Group the joined stream by room,
+         * 3. Accumulate a state for each room using aggregate(),
+         * 4. Filter only the notable state changes (overloaded or OK) from the aggregation stream to the output topic.
+         */
+        roomOccupancy
                 .outerJoin(roomCapacity, (occupancy, capacity) -> {
                     System.out.printf("joining on room: occupancy=%s, capacity=%s\n", occupancy, capacity);
-                    return new OccupancyCapacity(occupancy, capacity);
-//                    if (occupancy != null && capacity != null) { // Room exists for student
-//                        return occupancy > capacity ? String.valueOf(occupancy) : "OK";
-//                    } else {
-//                        return null;
-//                    }
-                }, Materialized.with(Serdes.String(), occCapSerde))
+                    return new RoomState(occupancy, capacity);
+                }, Materialized.with(Serdes.String(), stateSerde))
                 .toStream()
                 .groupByKey()
-                .aggregate(
-                        () -> state, /* zero */
-                        (room, newOccCap, roomState) -> {
-                            System.out.printf("aggregate: room=%s, newOccupancy=%s, newCapacity=%s prevState=%s\n",
-                                    room, newOccCap.occupancy, newOccCap.capacity, roomState.hashCode() + ", " + roomState.capacity + ", " + roomState.occupancy);
-
-                            // TODO: WTF the aggregate state isn't changing?? But it is changing in the output??
-                            // room=roomJ, newOccupancy=1, newCapacity=null state=RoomState{occupancy=0, capacity=0}
-                            // output newCapacity if: newOccupancy > newOccupancy
-                            // output OK if: newOccupancy <=
-
-                            final Long occupancy = newOccCap.occupancy;
-                            final Long capacity = newOccCap.capacity;
-
-                            if (occupancy != null && capacity != null) {
-                                // Room exists for student
-                                // OK if prevState is overloaded and this state isnt
-//                                if (occupancy > capacity) return;
-                                roomState.setOccupancy(occupancy);
-                                roomState.setCapacity(capacity);
-                            } else if (occupancy != null) {
-                                // Room doesn't exist (occupancy update only)
-                                roomState.setOccupancy(occupancy);
-                            } else if (capacity != null) {
-
-                                // Student doesn't exist for room (room capacity update only)
-                                roomState.setCapacity(capacity);
-                            }
-                            return roomState;
-//                        },
-//                        (room, oldOccCap, roomState) -> { /* subtractor */
-//                            System.out.printf("aggregate subtract: room=%s, oldOccupancy=%s, oldCapacity=%s state=%s\n",
-//                                    room, oldOccCap.occupancy, oldOccCap.capacity, roomState);
-//                            return new RoomState();
-                        },
-                        Materialized.with(Serdes.String(), roomStateSerde)
-                );
-
-
-        // TODO: filter out (room, OK) messages when the occupancy or max capacity didnt actually change
-        // ie: dont output OK every time a student gets added to a room that hasn't reached capacity yet
-        // Maybe aggregate by room to hold state of last occupancy
-        c.toStream().mapValues(RoomState::toString).filter((key, value) -> value != null).to(outputTopic);
-
-
-//
-//        builder.<String, String>stream(classroomTopic)
-//                .groupByKey()
-//                .count()
-//                .toStream()
-//                .map((key, value) -> KeyValue.pair(key, String.valueOf(value)))
-//                .to(outputTopic);
-
-
-//        KTable<String, Long> roomCapacity = builder.<String, String>table(classroomTopic)
-//                .mapValues((ValueMapper<String, Long>) Long::valueOf);
-
-//        KStream<String, String> students = builder.stream(studentTopic); // StudentId,RoomID
-//        KStream<String, String> rooms = builder.t(classroomTopic); // RoomId,MaxCapacity
-
-//        Serde<RoomState> roomStateSerde = Serdes.serdeFrom(new RoomStateSerializer(), new RoomStateDeserializer());
-
-//        roomTable.outerJoin(studentRoomTable.selectKey((student, room) -> room), (max, student) -> {
-//
-//        }, JoinWindows.of(Duration.ofSeconds(1)));
-//        students.map((student, room) -> KeyValue.pair(room, student))
-//                .outerJoin(rooms, (student, roomCap) -> {
-//                    if (student == null) return null;
-//                    return student;
-//                    // whatever stream just emitted is the one with a non-null value
-////                    System.out.printf("joining: student = %s, cap = %s\n", student, roomCap);
-////                    return KeyValue.pair(student, roomCap != null ? Long.valueOf(roomCap) : null);
-//                }, JoinWindows.of(Duration.ofSeconds(1)))
-//                .groupByKey();
-//                .mapValues(KeyValue::toString)
-//                .groupByKey()
-//                .aggregate(RoomState::new, (room, studentOrCapacity, roomState) -> {
-//                    String student = studentOrCapacity.key;
-//                    Long capacity = studentOrCapacity.value;
-//                    // Assert that at least one is non-null
-//                    assert !(student == null && capacity == null);
-//
-//                    if (student != null) { // Student entering the room
-//                        if (roomState.contains(student)) {
-//
-//                        }
-//                    } else { // Room capacity updated
-//
-//                    }
-//                }, Materialized.with(Serdes.String(), roomStateSerde))
-//                .reduce(new Reducer<String>() {
-//                    @Override
-//                    public String apply(String value1, String value2) {
-//                        return value1 + ", " + value2;
-//                    }
-//                })
-//                .toStream()
-//                .to(outputTopic);
-
-
-//        KStream<String, String> rooms = builder.stream(classroomTopic);
-//        students.join(rooms, (l, r) -> l + "/" + r, JoinWindows.of(Duration.ofSeconds(1))).to(outputTopic);
-
-//        builder.stream(classroomTopic).join(builder.stream(studentTopic), (a, b) -> a + " || " + b, JoinWindows.of(Duration.ofSeconds(1)))
-//                .to(outputTopic);
-//        students
-//                .selectKey((student, room) -> room)
-//                .join(roomCapacity, StudentCapacity::new)
-//                .groupByKey()
-//                .aggregate(RoomState::new, (key, value, state) -> {
-//                    System.out.printf("aggregate: k=%s, v=%s, state=%s\n", key, value.capacity + " " + value.student, state);
-//                    return state.enter(value.student);
-//                }, Materialized.with(Serdes.String(), roomStateSerde))
-//                .toStream()
-//                .to(outputTopic);
-
-//        aggregate.toStream().mapValues(RoomState::toString).to(outputTopic);
-//        rooms.join(students)
-//                .groupByKey()
-//                .aggregate(RoomState::new, (key, value, aggregate) -> null)
-
-
-//                .reduce(new Reducer<String>() {
-//            @Override
-//            public String apply(String value1, String value2) {
-//                return value1 + value2;
-//            }
-//        }).toStream().to(outputTopic);
-
-        /**
-         * TODO: Window the output to 1 second intervals b/c of this?
-         *
-         * 3. The application should be configured so that each input
-         message is consumed and processed in around one second or
-         less. For example, if the occupancy of a room increases beyond
-         the maximum capacity then the corresponding output record
-         should be committed to the output topic within around one
-         second. When the grading script commits a message to one of
-         the input topics, it will wait up to five seconds for the
-         application to commit a message to the output topic.
-         */
-        // add code here
-        //
-        // ... = builder.stream(studentTopic);
-        // ... = builder.stream(classroomTopic);
-        // ...
-        // ...to(outputTopic);
+                .aggregate(RoomState::new, (room, newState, prevState) -> {
+                    System.out.printf("aggregate: room=%s, newState=%s prevState=%s\n", room, newState, prevState);
+                    if (newState.isOverloaded()) {
+                        newState.setOutput(String.valueOf(newState.occupancy));
+                    } else if (newState.isOk() && prevState.isOverloaded()) {
+                        newState.setOutput("OK");
+                    }
+                    return newState;
+                }, Materialized.with(Serdes.String(), stateSerde))
+                .toStream()
+                .filter((room, state) -> state.hasOutput())
+                .mapValues(RoomState::getOutput)
+                .to(outputTopic);
 
 
         KafkaStreams streams = new KafkaStreams(builder.build(), props);
